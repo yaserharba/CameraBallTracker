adc742ce74452efdab4e5f7b3bf4109b49b2f3e9
Yaser Harba <49529460+yaserharba@users.noreply.github.com>
2021-08-06 15:20:11 -0700

    Update cameraProjectMatlab.m

--- cameraProjectMatlab.m
+++ cameraProjectMatlab.m
@@ -14,18 +14,8 @@ Errors = [0, 0];
 XE = [];
 YE = [];
 try
-    %[status, output] = system('C:\Users\YaserHarba\anaconda3\Scripts\activate base & python CameraProject.py')
-    %pause(0.1);
-    %status = system('D: &')
-    %pause(0.1);
-    %status = system('cd CameraProject &')
-    %pause(0.1);
     fig = CameraProjectFig;
     set(fig,'CloseRequestFcn',@my_closereq);
-    %title(handles.errors, 'Error State');
-    %xlabel(handles.errors, 'Time At Last 20 (Frames)');
-    %ylabel(handles.errors, 'Error (Pixel)');
-    %handles.errors.Clipping = 'on';
     writeRun(run);
     while run == 1
         PID = [kp ki kd];
@@ -34,16 +24,6 @@ try
         set(handles.KDIN,'String',kd);
         writePID(PID);
         writeColor(ballColor);
-        %Errors = readErrors(Errors);
-        %XE(end+1) = Errors(1);
-        %YE(end+1) = Errors(2);
-        %hold(handles.errors, 'on');
-        %plot(handles.errors, max(length(XE)-20,1):length(XE), XE(max(length(XE)-20,1):length(XE)), 'color', [1 0 0]);
-        %plot(handles.errors, max(length(YE)-20,1):length(YE), YE(max(length(YE)-20,1):length(YE)), 'color', [0 1 0]);
-        %axis(handles.errors, [max(length(YE)-20,0), length(YE), -240, 240]);
-        %line(handles.errors, [max(length(XE)-20,0) length(XE)],[0 0],'Color',[0 0 1]);
-        %legend(handles.errors, 'X Error','Y YError');
-        %hold(handles.errors, 'off');
         run = readRun(run);
         pause(0.5);
     end
